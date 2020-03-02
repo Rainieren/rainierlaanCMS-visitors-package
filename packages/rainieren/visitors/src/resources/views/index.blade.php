352b8c4b2912e388dd1bcd5435aac27b9f6745bf
Rainier Laan <rainier.laan@home.nl>
2020-03-02 20:55:11 +0100

    A complete mess

--- packages/rainieren/visitors/src/resources/views/index.blade.php
+++ packages/rainieren/visitors/src/resources/views/index.blade.php
@@ -9,49 +9,50 @@
         </div>
         <div class="row my-5">
             <div class="col-md-7">
-                <div class="card card-grey fadeInUp">
+                <div class="card card-grey fadeInUp h-100">
                     <div class="card-body">
                         <h5>{{ __('Visitors and Signups chart') }}</h5>
+                        <canvas id="visitorsSignupsChart" width="400" height="400"></canvas>
                     </div>
                 </div>
             </div>
             <div class="col-md-5">
                 <div class="row">
-                    <div class="col-md-6">
-                        <div class="card card-purple fadeInUp">
+                    <div class="col-md-6 mb-3">
+                        <div class="card card-purple fadeInUp h-100">
                             <div class="card-body">
-                                <h5>{{ __('Total visitors') }}</h5>
-                                <h2>632 294</h2>
+                                <h5 class="text-white">{{ __('Total visitors') }}</h5>
+                                <h2 class="text-white">632 294</h2>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="card card-purple fadeInUp">
+                    <div class="col-md-6 mb-3">
+                        <div class="card card-purple fadeInUp h-100">
                             <div class="card-body">
-                                <h5>{{ __('Visitors this month') }}</h5>
-                                <h2>235 843 <i class="far fa-arrow-alt-up"></i></h2>
+                                <h5 class="text-white">{{ __('Visitors this month') }}</h5>
+                                <h2 class="text-white">235 843 <i class="far fa-arrow-alt-up"></i></h2>
                             </div>
                             <div class="card-footer">
-                                <small class="m-0">Last month 222 000 <i class="far fa-arrow-alt-up"></i></small>
+                                <p class="m-0 text-white">Last month 222 000 <i class="far fa-arrow-alt-up"></i></p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="card card-purple fadeInUp">
+                    <div class="col-md-6 mb-3">
+                        <div class="card card-purple fadeInUp h-100">
                             <div class="card-body">
-                                <h5>{{ __('Total Signups') }}</h5>
-                                <h2>174 324</h2>
+                                <h5 class="text-white">{{ __('Total Signups') }}</h5>
+                                <h2 class="text-white">174 324</h2>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="card card-purple fadeInUp">
+                    <div class="col-md-6 mb-3">
+                        <div class="card card-purple fadeInUp h-100">
                             <div class="card-body">
                                 <h5>{{ __('Signups this month') }}</h5>
                                 <h2>23 284 <i class="far fa-arrow-alt-up"></i></h2>
                             </div>
                             <div class="card-footer">
-                                <small class="m-0">Last month 14 021 <i class="far fa-arrow-alt-up"></i></small>
+                                <p class="m-0 text-white">Last month 14 021 <i class="far fa-arrow-alt-up"></i></p>
                             </div>
                         </div>
                     </div>
